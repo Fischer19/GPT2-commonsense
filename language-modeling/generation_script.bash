ed47d7fbfffa5e6cd8108d50fcbd980d8b9b3163
Fischer19 <zhaojinyu19@163.com>
2020-08-20 11:35:42 -0700

    update script

--- language-modeling/generation_script.bash
+++ language-modeling/generation_script.bash
@@ -1,1 +1,1 @@
-python generation_script.py --model_type gpt2 --model_name_or_path model_conceptnet --length 100 --stop_token "<EOS>" --k 3 --num_return_sequences 1 --test_dir test_prompt.txt --output_dir results/test_conceptnet_model.txt
+python generation_script.py --model_type gpt2 --model_name_or_path model_conceptnet --length 100 --stop_token "<EOS>" --k 3 --num_return_sequences 1 --test_dir data/test_prompt.txt --output_dir results/test_conceptnet_model.txt
